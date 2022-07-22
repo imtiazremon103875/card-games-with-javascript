3b5ee4a14e32fd714e07abb7e372dfd8f5db8229
imtiaz remon <imtiazremon10375@gmail.com>
2022-07-22 12:34:34 +0600

    card loaded done

--- deck.js
+++ deck.js
@@ -1,6 +1,39 @@
-class Deck {
-    constructor(cards) {
+const SUITS = ["♠", "♣", "♥", "♦"]
+const VALUES = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+]
+
+export default class Deck {
+    constructor(cards = freshDeck()) {
         this.cards = cards
     }
+    shuffle() {
+        this.cards.sort((a, b) => Math.random() - .5)
+    }
+}
 
+class Card {
+    constructor(suit, value) {
+        this.suit = suit
+        this.value = value
+    }
+}
+function freshDeck() {
+    return SUITS.flatMap(suit => {
+        return VALUES.map(value => {
+            return new Card(suit, value)
+        })
+    })
 }
