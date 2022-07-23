60c3827c9dae6977be4adecd3bf0b11d3c66fc6e
imtiaz remon <imtiazremon10375@gmail.com>
2022-07-23 12:05:13 +0600

    js with card design

--- deck.js
+++ deck.js
@@ -39,8 +39,19 @@ class Card {
         this.suit = suit
         this.value = value
     }
+    get color() {
+        return this.suit === "♣" || this.suit === "♠" ? "black" : "red"
+    }
+    getHTML() {
+        const cardDiv = document.createElement('div')
+        cardDiv.innerText = this.suit
+        cardDiv.classList.add('card', this.color)
+        cardDiv.dataset.value = `${this.value} ${this.suit}`
+        return cardDiv
+    }
 
 }
+
 
 function freshDeck() {
     return SUITS.flatMap(suit => {
--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 import Deck from "./deck.js"
 
-const deck = new Deck()
-deck.shuffle()
-console.log(deck.cards)
+const computerCardSlot = document.querySelector(".computer-card-slot")
+const playerCardSlot = document.querySelector(".player-card-slot")
+const computerDeckElement = document.querySelector(".computer-deck")
+const playerDeckElement = document.querySelector(".player-deck")
+const text = document.querySelector(".text")
+
+let playerDeck, computerDeck
+startgame()
+
+function startgame() {
+    const deck = new Deck()
+    deck.shuffle()
+
+    const deckMidPoint = Math.ceil(deck.numberOfCards / 2)
+    playerDeck = new Deck(deck.cards.slice(0, deckMidPoint))
+    computerDeck = new Deck(deck.cards.slice(deckMidPoint, deck.numberOfCards))
+
+
+    cleanBeforeRound()
+
+}
+function cleanBeforeRound() {
+    computerCardSlot.innerHTML = ''
+    playerCardSlot.innerHTML = ''
+    text.innerHTML = ''
+    updateDeckCount()
+}
+function updateDeckCount() {
+    computerDeckElement.innerText = computerDeck.numberOfCards
+    playerDeckElement.innerText = playerDeck.numberOfCards
+}
+
+
+// computerCardSlot.appendChild(deck.cards[0].getHTML())
